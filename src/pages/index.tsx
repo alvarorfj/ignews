3377c6f07410ac73530e8b6705f284ef1eddb65c
alvarorfj <67553999+alvarorfj@users.noreply.github.com>
2022-08-01 21:45:23 -0300

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>IgNews</title>
+      </Head>
+      <h1>Ola Mundo!</h1>
+    </>
+  )
+}
