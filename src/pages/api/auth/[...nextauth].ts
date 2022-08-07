a04e1f9864dbcec963063c9e3fceebc28104b716
Alvaro <alvarorfj@gmail.com>
2022-08-07 00:06:15 -0300

    Adicionando integração com o FaunaDB

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,7 +1,9 @@
+import { query as q } from "faunadb";
+
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
 import GoogleProvider from "next-auth/providers/google";
-
+import { fauna } from '../../../services/fauna'
 export default NextAuth({
   // Configure one or more authentication providers
   providers: [
@@ -21,4 +23,42 @@ export default NextAuth({
           }
       })
   ],
+  jwt: {
+    secret: process.env.SIGNING_KEY,
+  },
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+        try{
+            if (account.provider === "google" || account.provider === "github") {
+                await fauna.query(
+                    q.If(
+                        q.Not(
+                            q.Exists(
+                                q.Match(
+                                    q.Index('user_by_email'),
+                                    q.Casefold(user.email)
+                                )
+                            )
+                        ),
+                        q.Create(
+                            q.Collection('users'),
+                            {data: {user:user, email: user.email, account: account, profile: profile, credentials: credentials}}
+                        ),
+                        q.Get(
+                            q.Match(
+                                q.Index('user_by_email'),
+                                q.Casefold(user.email)
+                            )
+                        )
+                    )
+                )
+                return true;
+            }
+            return false;
+        }catch(err) {
+            console.log(err)
+            return false;
+        }
+      }
+  }
 })
