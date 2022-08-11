5396d1d613bace1ad1c457ad1e092043c0490367
Alvaro <alvarorfj@gmail.com>
2022-08-10 22:54:44 -0300

    Integração com o Prismic

---
+++ .slicemachine/assets/slices/TextBlock/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/TextBlock';
+
+export default {
+  title: 'slices/TextBlock'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","version":"sktwi1xtmkfgx8626","items":[{"teste":[{"type":"paragraph","text":"Amet laboris commodo tempor deserunt pariatur enim aliqua voluptate anim laboris magna duis commodo ea. Enim ex elit quis elit voluptate minim culpa adipisicing sit eiusmod reprehenderit culpa.","spans":[]}]}],"primary":{"title":[{"type":"heading1","text":"Shallow","spans":[]}],"description":[{"type":"paragraph","text":"Ad mollit id duis. Exercitation in sit officia enim laborum ipsum ipsum culpa do ex non ullamco ullamco proident nisi. Commodo pariatur aliquip nostrud incididunt do aute deserunt tempor pariatur sunt ullamco commodo sit.","spans":[]}]},"slice_type":"text_block","id":"_Default"}} />
+_Default.storyName = ''
